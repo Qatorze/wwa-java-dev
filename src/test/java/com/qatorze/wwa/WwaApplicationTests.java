9ec4e93f603b82900ad123d824e46a39915d65b4
Cédric Vianney Mokoko Bayito <cedricbayito@gmail.com>
2024-12-01 07:21:42 +0100

    first commit

---
+++ src/test/java/com/qatorze/wwa/WwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qatorze.wwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
